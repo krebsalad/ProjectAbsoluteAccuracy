d84263f8556094e64d721c1bfc55eef19d5ec0a8
krebsalad <johanmgina7@gmail.com>
2018-09-27 16:40:08 +0200

    script

--- urdfMaker.py
+++ urdfMaker.py
@@ -3,23 +3,45 @@
 import os
 import subprocess
 import re
+import sys
+import glob
+
+# default startup arguments
+sysarg_ros_version = "kinetic"
+sysarg_robot_name = "irb1600"
+sysarg_project_name = "catkin_ws"
+sysarg_project_folder_dir = "/home/"+"turtle/"
+
+# help for options
+if(sys.argv[1] == "help"):
+    print("Options: robot_name:=<name>, project_name:=<project_name>, ros_version:=<kinetic/indigo/>")
+
+# options
+for arg in sys.argv:
+    if(re.match("robot_name:=", arg)):
+        sysarg_robot_name = re.sub("robot_name:=", "", arg)
+    if(re.match("project_name:=", arg)):
+        sysarg_project_name = re.sub("project_name:=", "", arg)
+    if(re.match("ros_version:=", arg)):
+        sysarg_ros_version = re.sub("ros_version:=", "", arg)
 
 # ROS args
-arg_distribution_version = "kinetic"
+arg_distribution_version = sysarg_ros_version
 arg_ros_sources = 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 arg_keyp1 = 'hkp://ha.pool.sks-keyservers.net:80'
 arg_keyp2 = '421C365BD9FF1F717815A3895523BAEEB01FA116'
 arg_ros_distribution = "ros-"+arg_distribution_version+"-desktop-full"
 
 # project args
-arg_robot_package_name = "robot_irb1600"
+arg_robot_package_name = "robot_"+sysarg_robot_name
+arg_robot_support_package = "abb_"+sysarg_robot_name+"_support"
 
 # project paths
-pth_base = "/home/turtle/"
+pth_base = sysarg_project_folder_dir
 pth_bashrc = pth_base+".bashrc"
 
-pth_catkin_workspace = pth_base+"catkin_ws/"
-pth_project_packages = pth_base+"catkin_ws/src/"
+pth_catkin_workspace = pth_base+sysarg_project_name+"/"
+pth_project_packages = pth_catkin_workspace+"src/"
 pth_pkg_robot = pth_project_packages+arg_robot_package_name+"/"
 
 # ROS paths
@@ -54,31 +76,39 @@ subprocess.call(["git", "clone", lnk_abb_git, pth_ros_downloads+"/abb"])
 subprocess.call(["git", "clone", lnk_abb_experimental_git, pth_ros_downloads+"/abb_experimental"])
 subprocess.call(["git", "clone", lnk_industrial_core_git, pth_ros_downloads+"/industrial_core"])
 
-# create package (create pkg not yet working)
+# create package 
 subprocess.call(["catkin_create_pkg", arg_robot_package_name, "roscpp"], cwd=pth_project_packages)
 subprocess.call(["catkin_make"], shell=True, cwd=pth_catkin_workspace)
 
-subprocess.call(["mkdir", "-p", pth_pkg_robot+"src/"])
+# subprocess.call(["mkdir", "-p", pth_pkg_robot+"src/"])
 subprocess.call(["mkdir", "-p", pth_pkg_robot+"config/"])
 subprocess.call(["mkdir", "-p", pth_pkg_robot+"launch/"])
 subprocess.call(["mkdir", "-p", pth_pkg_robot+"meshes/"])
 subprocess.call(["mkdir", "-p", pth_pkg_robot+"urdf/"])
 
 # copy files for needed for the urdf to build a kinematic model
-subprocess.call(["cp", pth_ros_downloads+"abb_experimental/abb_irb1600_support/urdf/irb1600_6_12_macro.xacro", pth_pkg_robot+"urdf/"+arg_robot_package_name+".xacro"])
-subprocess.call(["cp", "-r", pth_ros_downloads+"abb_experimental/abb_irb1600_support/meshes/irb1600", pth_pkg_robot+"meshes/"])
-subprocess.call(["cp", "-r", pth_ros_downloads+"abb_experimental/abb_irb1600_support/meshes/irb1600_6_12", pth_pkg_robot+"meshes/"])
+arg_macro_xacro = ""
+for file in glob.glob(pth_ros_downloads+"abb_experimental/"+arg_robot_support_package+"/urdf/"+sysarg_robot_name+"*"+"_macro.xacro"):
+    arg_macro_xacro = re.sub(pth_ros_downloads+"abb_experimental/"+arg_robot_support_package+"/urdf/", "", file)
+
+subprocess.call(["cp", pth_ros_downloads+"abb_experimental/"+arg_robot_support_package+"/urdf/"+arg_macro_xacro, pth_pkg_robot+"urdf/"+arg_robot_package_name+".xacro"])
+subprocess.call(["cp", "-r", pth_ros_downloads+"abb_experimental/"+arg_robot_support_package+"/meshes", pth_pkg_robot])
+subprocess.call(["cp", "-r", pth_ros_downloads+"abb_experimental/"+arg_robot_support_package+"/meshes", pth_pkg_robot])
 
 # loop through each line in urdf and change/subsitute/remove uneeded lines
+arg_abb_irb_name = re.sub("_macro.xacro", "", arg_macro_xacro)
 with open(pth_pkg_robot+"urdf/"+arg_robot_package_name+".xacro") as f:
     urdf_file = open(pth_pkg_robot+"urdf/"+arg_robot_package_name+"_macro.urdf", "w+")
     for line in f:
-        line = re.sub(r'\babb_irb1600_support\b', arg_robot_package_name, line)
-        line = re.sub('<xacro:macro name="abb_irb1600_6_12" params="prefix">', "", line)
-        line = re.sub('<robot xmlns:xacro="http://ros.org/wiki/xacro" name="irb1600_6_12">', '<robot name="'+arg_robot_package_name+'" xmlns:xacro="http://ros.org/wiki/xacro">', line)
+        line = re.sub(arg_robot_support_package, arg_robot_package_name, line)
+        line = re.sub('<xacro:macro name="'+arg_abb_irb_name+'" params="prefix">', "", line)
+        line = re.sub('<robot xmlns:xacro=.*?>', '<robot name="'+arg_robot_package_name+'" xmlns:xacro="http://ros.org/wiki/xacro">', line)
+        line = re.sub('<xacro:macro.*?>', "", line)
         line = re.sub('</xacro:macro>', "", line)
+        line = re.sub('<xacro:include.*?>', "", line)
         line = re.sub('prefix', '',line)
         line = re.sub(r'[$]'+r'{}', '', line)
+        line = re.sub('<xacro:material.*?>', '<material name="yellow">\n<color rgba="0 1 1 1"/>\n</material>', line)
         urdf_file.write(line)
     
     urdf_file.close()
@@ -92,3 +122,4 @@ with open(pth_pkg_robot+"urdf/"+arg_robot_package_name+".urdf", "wb+", 0) as out
 subprocess.call(["catkin_make"], shell=True, cwd=pth_catkin_workspace)
 
 # Done
+print("done creating project "+arg_robot_package_name+ " with urdf for "+arg_abb_irb_name+". The urdf is ready for MoveIT! setup assistant. Do not forget to source you project before using setupassistant, go into: "+pth_catkin_workspace+" and do: source devel/setup.bash")
