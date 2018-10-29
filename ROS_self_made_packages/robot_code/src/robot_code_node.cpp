1537d6b5c020b020bf8cadc2412637f758d5f88b
krebsalad <johanmgina7@gmail.com>
2018-10-29 09:44:03 +0100

    update

--- ROS_self_made_packages/robot_code/src/robot_code_node.cpp
+++ ROS_self_made_packages/robot_code/src/robot_code_node.cpp
@@ -2,15 +2,50 @@
 
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 int main(int argc, char* argv[])
 {
     //ros ini, ros node and spinner setup
     ros::init(argc, argv, "robot_code");
-    //ros::NodeHandle node;
+    ros::NodeHandle node;
 
     ros::AsyncSpinner spinner(1);
     spinner.start();
+
+    //target pose variables
+    double w;
+    double x;
+    double y;
+    double z;
+
+    //arguments to target pose variables
+    std::stringstream s_w;
+    std::stringstream s_x;
+    std::stringstream s_y;
+    std::stringstream s_z;
+
+    if(argv[4] != nullptr)
+    {      
+        s_w << argv[1];
+        s_w >> w;
+        
+        s_x << argv[2];
+        s_x >> x;
+
+        s_y << argv[3];
+        s_y >> y;
+
+        s_z << argv[4];
+        s_z >> z;
+    }
+    else
+    {
+        ROS_INFO_NAMED("irb1600", "not enough arguments given");
+        return 0;
+    }
 
    //move group setup
     static const std::string planning_group = "group1";
@@ -21,18 +56,13 @@ int main(int argc, char* argv[])
     //Plan a new path using a pose/ !not executing yet
     geometry_msgs::Pose target_pose1;
     int input;
-    std::cout << "orientation: \n";
-    std::cin >> input;
-    target_pose1.orientation.w = input;
-    std::cout << "orientation: \n";
-    std::cin >> input;
-    target_pose1.position.x = input;
-    std::cout << "orientation: \n";
-    std::cin >> input;
-    target_pose1.position.y = input;
-    std::cout << "orientation: \n";
-    std::cin >> input;
-    target_pose1.position.z = input;
+    target_pose1.orientation.w = w;
+
+    target_pose1.position.x = x;
+
+    target_pose1.position.y = y;
+
+    target_pose1.position.z = z;
    
     //set the target for the next path
     move_group.setPoseTarget(target_pose1);
@@ -51,6 +81,6 @@ int main(int argc, char* argv[])
     {
         ROS_INFO_NAMED("irb1600", "failed planning the path");
     }
-
+    
     return 0;
 }
